240897fc646d3847ab18b4460363566ee1679d40
Ashmeet <ashmeet.kalsi.ug21@nsut.ac.in>
2026-06-16 11:37:36 +0530

    Add server folder

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./db");
+const app = express();
+const PORT = process.env.PORT || 5500;
+const authRoutes = require("./routes/authRoutes");
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Working");
+});
+
+app.get("/api/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ success: true, time: result.rows[0].now });
+  } catch (err) {
+    console.error("DB Error:", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
